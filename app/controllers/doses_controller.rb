05bd77f35aa35c2592e8e5a6ae0b1c5fc97a4968
julie laloy <julie.laloy@gmail.com>
2021-03-04 17:33:51 +0100

    a

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -26,7 +26,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
